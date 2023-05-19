8aea485a813415b1747e021fae39a86e4c0c58cc
Shanto57575 <shanto57575@gmail.com>
2023-05-19 14:46:34 +0600

    finally complete

---
+++ javascript/chat.js
@@ -0,0 +1,63 @@
+const form = document.querySelector(".typing-area"),
+incoming_id = form.querySelector(".incoming_id").value,
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+}
+
+inputField.focus();
+inputField.onkeyup = ()=>{
+    if(inputField.value != ""){
+        sendBtn.classList.add("active");
+    }else{
+        sendBtn.classList.remove("active");
+    }
+}
+
+sendBtn.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              inputField.value = ""; //once message is inserted into database then leave blank the input field
+              scrollToBottom();
+          }
+      }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+setInterval(() =>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+            let data = xhr.response;
+            chatBox.innerHTML = data;
+            if(!chatBox.classList.contains("active")){
+                scrollToBottom();
+              }
+          }
+      }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //The Content-type request header specifies the MIME type of the request body, and the value application/x-www-form-urlencoded indicates that the request body is URL-encoded form data. URL-encoded form data is a format commonly used to submit HTML form data to a server.//This line of code might be used in a script that sends an HTTP request with a request body that contains form data that has been URL-encoded.
+    xhr.send("incoming_id="+incoming_id);
+}, 500);
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+  
